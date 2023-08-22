009762afe6cb25d8fba575d7240c53b6d3e890ea
Iuri Johansson <iuri.johansson@gmail.com>
2023-08-22 15:47:54 +0000

    Update necessary changes to ALLOWED_HOST and CLIENT_ORIGIN_DEV work well with different URLs and grant access to deployed API during developing frontend app in Gitpod

--- bd_backend/settings.py
+++ bd_backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import re
 from pathlib import Path
 import os
 import dj_database_url
@@ -60,7 +60,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-iurjoh-bdbackend-7duf0d2wqsz.ws-eu104.gitpod.io', 'iurjoh-baby-daily-backend-api.herokuapp.com']
+ALLOWED_HOSTS = ['8000-iurjoh-bdbackend-7duf0d2wqsz.ws-eu104.gitpod.io', 'ALLOWED_HOSTS']
 
 
 # Application definition
@@ -107,9 +107,10 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
